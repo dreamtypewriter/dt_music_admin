4ab3fff3f5bf815bc940d87029af20afc8b67138
dreamtypewriter <2741049767@qq.com>
2021-09-09 11:19:54 +0800

    try fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,14 @@
 module.exports = {
     NODE_ENV: '"production"',
-    api: '//localhost:8090'
+    api: '//localhost:8090',
+    proxyTable: {
+        '/api': {
+            // 替换服务地址
+            target: "http://localhost:8090",
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
+        }
+    }
 }
